8fb3be6eb585adaea07703ee4437e244faaf2638
Nathaniel <nathanwriting@126.com>
2022-05-15 19:11:31 +0800

    更新逻辑

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -3,7 +3,8 @@ import 'package:admin/screens/dashboard/components/my_fields.dart';
 import 'package:flutter/material.dart';
 
 import '../../constants.dart';
-import 'components/header.dart';import 'components/recent_files.dart';
+import 'components/header.dart';
+import 'components/recent_files.dart';
 import 'components/storage_details.dart';
 
 class DashboardScreen extends StatelessWidget {
