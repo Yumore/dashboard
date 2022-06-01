3d732b0d2f1250b76d001e9cd2158acb0fe01970
Nathaniel <nathanwriting@126.com>
2022-06-01 17:47:47 +0800

    更新代码

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -2,9 +2,7 @@
 // Generated file. Do not edit.
 //
 
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-
-// ignore_for_file: directives_ordering
+import 'package:flutter_web_plugins/flutter_web_plugins.dart'; // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 // ignore_for_file: depend_on_referenced_packages
 
